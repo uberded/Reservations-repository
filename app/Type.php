17fa04320ebdf3d047096738348a0cd1bab6d27c
uberded <uberded@ex-t.jp>
2016-11-05 23:41:38 +0900

    オブジェクトの種類に関する追加

---
+++ app/Type.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Type extends Model
+{
+
+    // テーブル設定
+    protected $table = 'object_types';
+
+    // ソフトデリートを有効化し日付へキャスト
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
+
+}
---
+++ database/migrations/2016_11_05_232934_create_object_types_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateObjectTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('object_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Type')->uniquie();
+            $table->integer('Register')->unsigned();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('object_types');
+    }
+}
