cc23d94c3f0abd9da900cef0632221fa0f2566d9
uberded <uberded@ex-t.jp>
2016-11-05 22:53:51 +0900

    API用ルーティングを追加 不格好のため変更の余地あり

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,16 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+//Route::get('/user', function (Request $request) {
+ //   return $request->user();
+//})->middleware('auth:api');
+
+Route::get('', 'Object/APIController@showMinecraftVersions');
+
+Route::get('/{MinecraftVersion}', 'Object/APIController@showObjectType');
+
+Route::get('/{MinecraftVersion}/{ObjectType}', 'Object/APIController@showObjects');
+
+Route::get('/{MinecraftVersion}/{ObjectType}/{ObjectName}', 'Object/APIController@showObjectVersions');
+
+Route::get(('/{MinecraftVersion}/{ObjectType}/{ObjectName}/{ObjectVersion}', 'Object/APIController@showObjectData');
