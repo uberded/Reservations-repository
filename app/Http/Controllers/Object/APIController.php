e4e79f83350ea4c7086498c8792fc01c919e028a
uberded <uberded@ex-t.jp>
2016-11-06 00:57:55 +0900

    API部分実装 今後調整の可能性あり

--- app/Http/Controllers/Object/APIController.php
+++ app/Http/Controllers/Object/APIController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Response;
 use App\Minecraft;
+use App\Type;
+use App\Object;
+use App\Detail;
 
 class APIController extends Controller
 {
@@ -27,6 +30,96 @@ class APIController extends Controller
             $Data->put('Minecraft Version', $Versions);
             // データを返す
             return $this::UnescapedResponse($Data);
+        }
+    }
+
+    /**
+    * オブジェクトの種類を表示します
+    *
+    * @param Request $MinecraftVersion
+    * @return Response
+    */
+    public function showObjectType($MinecraftVersion)
+    {
+        // 全種類を取得
+        $Types = Type::all();
+        // 一切ない場合のみ404
+        if($Types->isEmpty()) return Response::json(array('status' => 'Object Type has been unregistered.'), 404);
+        else{
+            // データの先頭にナビゲーションを挿入
+            $Data = collect(['status' => 'Object Type nav Index']);
+            $Data->put('Object Type', $Types);
+            // データを返す
+            return $this::UnescapedResponse($Data);
+        }
+    }
+
+    /**
+    * オブジェクトを表示します
+    *
+    * @param Request $MinecraftVersion, Request $ObjectType
+    * @return Response
+    */
+    public function showObjects($MinecraftVersion, $ObjectType)
+    {
+        // 条件にあたる全件を取得
+        $Objects = Object::where('MinecraftVersion', $MinecraftVersion)->where('ObjectType', $ObjectType)->get();
+        // 一切ない場合のみ404
+        if($Objects->isEmpty()) return Response::json(array('status' => $ObjectType . ' has been unregistered.'), 404);
+        else{
+            // データの先頭にナビゲーションを挿入
+            $Data = collect(['status' => $ObjectType . ' nav Index']);
+            $Data->put($ObjectType, $Objects);
+            // データを返す
+            return $this::UnescapedResponse($Data);
+        }
+    }
+
+    /**
+    * オブジェクトのバージョンを表示します
+    *
+    * @param Request $MinecraftVersion, Request $ObjectType, Request $ObjectName
+    * @return Response
+    */
+    public function showObjectVersions($MinecraftVersion, $ObjectType, $ObjectName)
+    {
+        // 条件にあたる一件を検索
+        $Object = Object::where('MinecraftVersion', $MinecraftVersion)->where('ObjectType', $ObjectType)->where('ObjectName', $ObjectName)->first();
+        // 一切ない場合のみ404
+        if($Object === null) return Response::json(array('status' => $ObjectName . ' has been unregistered.'), 404);
+        else{
+            // オブジェクトIDからデータを検索
+            $Versions = Detail::where('ObjectId', $Object->id)->value('Version');
+            // データの先頭にナビゲーションを挿入
+            $Data = collect(['status' => $ObjectName . ' Version nav Index']);
+            $Data->put('Version', $Versions);
+            // データを返す
+            return $this::UnescapedResponse($Data);
+        }
+    }
+
+    /**
+    * オブジェクトの詳細情報を表示します
+    * @param Request $MinecraftVersion, Request $ObjectType, Request $ObjectName, Request $ObjectVersion
+    * @return Response
+    */
+    public function showObjectData($MinecraftVersion, $ObjectType, $ObjectName, $ObjectVersion)
+    {
+        // 条件にあたる一件を検索
+        $Object = Object::where('MinecraftVersion', $MinecraftVersion)->where('ObjectType', $ObjectType)->where('ObjectName', $ObjectName)->first();
+        // 一切ない場合のみ404
+        if($Object === null) return Response::json(array('status' => $ObjectName . ' has been unregistered.'), 404);
+        else{
+            // オブジェクトIDからデータを検索
+            $Detail = Detail::where('ObjectId', $Object->id)->where('Version', $ObjectVersion)->value('Data');
+            if($Detail === null) return Response::json(array('status' => $ObjectName . ' ' . $ObjectVersion .' has been unregistered.'), 404);
+            else{
+                // データの先頭にナビゲーションを挿入
+                $Data = collect(['status' => $ObjectName . ' ' . $ObjectVersion . ' Detail nav Index']);
+                $Data->put('Detail', $Detail);
+                // データを返す
+                return $this::UnescapedResponse($Data);
+            }
         }
     }
 
