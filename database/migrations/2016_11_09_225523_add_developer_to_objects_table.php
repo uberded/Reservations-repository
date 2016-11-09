348ee1051567365e8e080f14a105a0744faa02af
uberded <uberded@ex-t.jp>
2016-11-09 22:58:32 +0900

    テーブルの説明項目の名前を変更、開発者名項目を追加

--- database/migrations/2016_11_09_225523_add_developer_to_objects_table.php
+++ database/migrations/2016_11_09_225523_add_developer_to_objects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddDescriptionToObjectsTable extends Migration
+class AddDeveloperToObjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddDescriptionToObjectsTable extends Migration
     public function up()
     {
         Schema::table('objects', function(Blueprint $table){
-            $table->string('Description');
+            $table->string('Developer');
         });
     }
 
@@ -26,7 +26,7 @@ class AddDescriptionToObjectsTable extends Migration
     public function down()
     {
         Schema::table('objects', function(Blueprint $table){
-            $table->dropColumn('Description');
-        });
+            $table->dropColumn('Developer');
+        }):
     }
 }
